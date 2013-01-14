a105c8d74ee8f96a0e3107b8df58a872fa72824e
Christian Kjær Laustsen <christianlaustsen@gmail.com>
2013-01-14 21:27:45 +0100

    Add menu options. Clean up. Use the NSUser object to store settings. Start implementing the preferences window.

--- KNetConnect.py
+++ KNetConnect.py
@@ -1,16 +1,30 @@
+"""
+Author: Christian Laustsen
+License: BSD (See accompanying LICENSE file)
+
+Description: Create a menulet in the system tray, and automate the process of connecting to 
+the K-Net and logging in to the server with ssh.
+
+"""
+
+import urllib2
+import webbrowser
+import paramiko
+import threading
 import objc, re, os
 from Foundation import *
 from AppKit import *
 from PyObjCTools import NibClassBuilder, AppHelper
-import urllib2
-import webbrowser
-import paramiko
+from PyObjCTools.KeyValueCoding import *
+
+import preferences
+
 
 DEBUG = False
 
 KNET_SERVER = 'fw2.k-net.dk'
-USERNAME = 'username'
-PASSWORD = 'password'
+USERNAME = getKey(preferences.defaults, u'username')
+PASSWORD = getKey(preferences.defaults, u'password')
 
 STATUS_IMAGES = {
     'idle': 'images/idle.png',
@@ -34,7 +48,6 @@ def testInternetConnection(testIPs):
         except ValueError:
             pass
     return (False, "")
-    
 
 def connectToKNet():
     if DEBUG: print "Trying to connect to KNet..."
@@ -44,16 +57,29 @@ def connectToKNet():
     stdin, stdout, stderr = ssh.exec_command('ls')
     return ssh
 
+def invokePreferences():
+    thread = threading.Thread(
+        target=preferences.Preferences()
+    )
+    thread.start()
+
 
 class Timer(NSObject):
+    """
+    Create a menulet in the system tray, and automate the process of connecting to 
+    the K-Net and logging in to the server with ssh.
+    
+    """
     
     images = {}
     statusbar = None
-    internet = False
+    menuitems = {}
+    checkForConnection = True
     ssh = None
     state = 'idle'
     
     def applicationDidFinishLaunching_(self, notification):
+        """Initialize the application, putting an icon in the system tray, etc."""
         self.statusbar = NSStatusBar.systemStatusBar()
         self.statusitem = NSStatusBar.systemStatusBar().statusItemWithLength_(NSVariableStatusItemLength)
         self.statusitem.setHighlightMode_(1)
@@ -64,74 +90,102 @@ class Timer(NSObject):
         self.startTimer()
     
     def setState(self, state):
+        """Set the state and change the picture if there is one."""
         if self.state != state:
             self.state = state
             if self.state in self.images:
                 self.statusitem.setImage_(self.images[self.state])
     
+    def closeConnection(self):
+        """Close the ssh connection."""
+        try:
+            self.ssh.close()
+        except AttributeError:
+            pass
+    
     def setUpImages(self):
+        """Make objects of all the images, and store them in the instance variable."""
         images = {}
         for i in STATUS_IMAGES.keys():
             images[i] = NSImage.alloc().initByReferencingFile_(STATUS_IMAGES[i])
         return images
     
     def addMenuItems(self, statusitem):
+        """Add menut items to the menu object."""
         items = [
-            ['Preferences...', 'preferences:', ''],
-            ['Help Center', 'helpcenter:', ''],
+            ['connection', 'Connecting...', '', ''],
             [],
-            ['Quit KNetConnect', 'terminate:', '']
+            ['pause', 'Stop Connecting', 'pause:', ''],
+            [],
+            ['preferences', 'Preferences...', 'preferences:', ''],
+            ['helpcenter', 'Help Center', 'helpcenter:', ''],
+            [],
+            ['quit', 'Quit                ', 'terminate:', '']
         ]
         menu = NSMenu.alloc().init()
         for item in items:
             if item:
-                name, action, key = item
+                keyName, name, action, key = item
                 menuItem = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_(name, action, key)
+                self.menuitems[keyName] = menuItem
             else:
                 menuItem = NSMenuItem.separatorItem()
             menu.addItem_(menuItem)
         statusitem.setMenu_(menu) # Bind it to the status item
     
     def startTimer(self):
+        """Execute the tick_() function on a regular interval."""
         self.timer = NSTimer.alloc().initWithFireDate_interval_target_selector_userInfo_repeats_(
             START_TIME, 10.0, self, 'tick:', None, True
         )
         NSRunLoop.currentRunLoop().addTimer_forMode_(self.timer, NSDefaultRunLoopMode)
-        self.timer
+        self.timer.fire()
     
     def checkConnection(self):
+        """Check if we are connected to the internet, or if we need to log in to K-Net."""
         conn, resp = testInternetConnection(CONNECTION_TEST_SITES)
         isKNet = "K-Net Login" in resp
         if conn and not isKNet:
-            self.internet = True
+            self.menuitems['connection'].setTitle_('Connected')
             self.setState('active')
         elif isKNet:
+            self.menuitems['connection'].setTitle_('Connecting...')
             self.setState('idle')
-            self.internet = False
-            try:
-                self.ssh.close()
-            except AttributeError:
-                pass
+            self.closeConnection()
             self.ssh = connectToKNet()
         else:
-            self.internet = False
+            self.menuitems['connection'].setTitle_('No Internet')
             self.setState('idle')
     
     def tick_(self, notification):
+        """This gets executed on a regular interval."""
         if DEBUG: print self.state
-        self.checkConnection()
+        if self.checkForConnection:
+            self.checkConnection()
+    
+    def pause_(self, notification):
+        """Stop/Start checking if we are connected."""
+        if self.checkForConnection:
+            self.checkForConnection = False
+            self.menuitems['pause'].setTitle_('Resume Connecting')
+            self.setState('idle')
+            self.closeConnection()
+        else:
+            self.checkForConnection = True
+            self.menuitems['connection'].setTitle_('Connecting...')
+            self.menuitems['pause'].setTitle_('Stop Connecting')
+    
+    #def preferences_(self, notification):
+    #    invokePreferences()
     
     def helpcenter_(self, notification):
         """Opens a webbrowser and directs to the github README."""
         webbrowser.open('https://github.com/Tehnix/KNetConnect', new=2)
-
+        
 
 if __name__ == "__main__":
     app = NSApplication.sharedApplication()
     delegate = Timer.alloc().init()
     app.setDelegate_(delegate)
     AppHelper.runEventLoop()
-    try:
-        app.ssh.close()
-    except AttributeError:
-        pass
+    app.closeConnection()
---
+++ preferences.py
@@ -0,0 +1,27 @@
+from Tkinter import *
+import threading
+from Foundation import *
+from AppKit import *
+from PyObjCTools import NibClassBuilder, AppHelper
+from PyObjCTools.KeyValueCoding import *
+
+
+dictionary = {}
+dictionary[u'username'] = u'pbkxxxx.xxx'
+dictionary[u"password"] = u'xxxxxxxx'
+NSUserDefaultsController.sharedUserDefaultsController().setInitialValues_(dictionary)
+
+defaults = NSUserDefaultsController.sharedUserDefaultsController().values()
+
+
+class Preferences(threading.Thread):
+    
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("KNetConnect Preferences")
+        self.window.resizable(width=FALSE, height=FALSE)
+        self.window.geometry("400x200+500+250")
+        self.window.mainloop()
+
+if __name__ == '__main__':
+    Preferences()
